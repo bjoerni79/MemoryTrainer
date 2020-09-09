c3901b73953ab3a5d4c12ffa3c1d2ec8f86f7817
Björn <bhillebrand79@gmail.com>
2020-09-09 14:26:05 +0200

    Bugfixes and Improvements in the Result overview UI and VM

---
+++ src/MemoryTrainer/Misc/IntegerToImageConverter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace MemoryTrainer.Misc
+{
+    public class IntegerToImageConverter : IValueConverter
+    {
+        private readonly string path = @"/Misc/Images/";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int recallState;
+            var isInteger = Int32.TryParse(value.ToString(), out recallState);
+
+            if (isInteger)
+            {
+                string uriString = null;
+
+                if (recallState == 1)
+                {
+                    uriString = path + "OK.png";
+                }
+
+                if (recallState == 2)
+                {
+                    uriString = path + "NotOK.png";
+                }
+
+                if (uriString != null)
+                {
+                    var uri = new Uri(uriString, UriKind.RelativeOrAbsolute);
+                    var imageStream = Application.GetResourceStream(uri);
+
+                    // https://docs.microsoft.com/en-us/dotnet/api/system.windows.media.imaging.bitmapdecoder?view=netcore-3.1
+                    BitmapDecoder decoder = BitmapDecoder.Create(imageStream.Stream, BitmapCreateOptions.None, BitmapCacheOption.Default);
+
+                    imageStream.Stream.Close();
+                    return decoder.Frames[0];
+                }
+
+                return null;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
--- src/MemoryTrainer/ViewModel/ResultOverviewViewModel.cs
+++ src/MemoryTrainer/ViewModel/ResultOverviewViewModel.cs
@@ -5,13 +5,19 @@ using MemoryTrainer.MVVM;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
+using System.Windows.Documents;
 
 namespace MemoryTrainer.ViewModel
 {
     public class ResultOverviewViewModel : ViewModelBase
     {
         private ResultOverview resultOverview;
+        private PAOResultOverview currentResultOverview;
+        private Dictionary<int, PAOResult> resultDict;
+
+        private bool isDebugModeEnabled = true;
 
         public ResultOverviewViewModel()
         {
@@ -25,7 +31,18 @@ namespace MemoryTrainer.ViewModel
 
         public IRefreshCommand Refresh { get; private set; }
 
-        public PAOResultOverview CurrentResultOverview { get; set; }
+        public PAOResultOverview CurrentResultOverview
+        {
+            get
+            {
+                return currentResultOverview;
+            }
+            set
+            {
+                currentResultOverview = value;
+                RenderCards();
+            }
+        }
 
         public ObservableCollection<PAOResultOverview> Results { get; set; }
 
@@ -36,22 +53,87 @@ namespace MemoryTrainer.ViewModel
             InternalClose();
         }
 
+        private void RenderCards()
+        {
+            if (CurrentResultOverview != null)
+            {
+                var id = CurrentResultOverview.Id;
+                var containsKey = resultDict.ContainsKey(id);
+                if (containsKey)
+                {
+                    var result = resultDict[id];
+                    var cards = result.Items;
+                    var tempList = new List<PAOResultItem>();
+
+                    foreach (var curItem in cards)
+                    {
+                        var paoResultItem = new PAOResultItem()
+                        {
+                            Person = curItem.Person,
+                            Action = curItem.Action,
+                            Object = curItem.Object,
+                            RecallState = curItem.RecallState
+                        };
+
+                        tempList.Add(paoResultItem);
+                    }
+
+                    SelectedCards = new ObservableCollection<PAOResultItem>(tempList);
+                    RaisePropertyChange("SelectedCards");
+                }
+            }
+        }
+
         private void OnRefresh()
         {
             var facade = new ContainerFacade();
             resultOverview = facade.Get<ResultOverview>(Bootstrap.Results);
             if (resultOverview != null)
             {
+                // Create a list and and an ID integer to play with later.
                 var list = new List<PAOResultOverview>();
                 int id = 0;
-                foreach (var paoResult in resultOverview.PAOResults)
+
+                // Use a temporary list instead of the enumeration of the instance, because we need to add tests items maybe.
+                resultDict = new Dictionary<int, PAOResult>();
+                var results = new List<PAOResult>();
+                results.AddRange(resultOverview.PAOResults);
+
+                //
+                //  Create some fake results for debugging if enabled
+                //
+                if (isDebugModeEnabled)
+                {
+                    // testResult1
+                    var testResult1 = new PAOResult
+                    {
+                        Comment = "Test Result 1",
+                        DeckTitle = "Test Deck 1"
+                    };
+                    testResult1.Add(new PAOResultItem { Person = PlayingCard.Diamond_Jack, Action = PlayingCard.Diamond_Queen, Object = PlayingCard.Diamond_King, RecallState = 0 });
+                    testResult1.Add(new PAOResultItem { Person = PlayingCard.Heart_Jack, Action = PlayingCard.Heart_Queen, Object = PlayingCard.Heart_King, RecallState = 1 });
+                    testResult1.Add(new PAOResultItem { Person = PlayingCard.Spade_Jack, Action = PlayingCard.Spade_Queen, Object = PlayingCard.Spade_King, RecallState = 2 });
+
+
+                    // testResult2
+                    var testResult2 = new PAOResult();
+                    testResult2.Comment = "Test Result 2";
+                    testResult2.DeckTitle = "Test Deck 2";
+                    testResult2.Add(new PAOResultItem { Person = PlayingCard.Diamond_2, Action = PlayingCard.Diamond_3, Object = PlayingCard.Diamond_4 });
+
+                    results.Add(testResult1);
+                    results.Add(testResult2);
+                }
+
+                foreach (var paoResult in results)
                 {
                     var paoResultOverview = new PAOResultOverview(id);
                     paoResultOverview.Comment = paoResult.Comment;
                     paoResultOverview.Deck = paoResult.DeckTitle;
 
-                    id++;
                     list.Add(paoResultOverview);
+                    resultDict.Add(id, paoResult);
+                    id++;
                 }
 
                 // Refresh the UI
