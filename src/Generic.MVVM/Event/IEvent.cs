14e455a3fb82ce8d94058a64653c2474c14d91b7
Björn <bhillebrand79@gmail.com>
2021-01-14 14:39:33 +0100

    Some ideas about Event mechanism

---
+++ src/Generic.MVVM/Event/IEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic.MVVM.Event
+{
+    public interface IEvent
+    {
+        void AddListener(IEventListener listener);
+        string EventId { get; }
+
+        Task TriggerAsync();
+        void Clear();
+    }
+}
---
+++ src/Generic.MVVM/Event/IEventListener.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic.MVVM.Event
+{
+    public interface IEventListener
+    {
+        void OnTrigger(string eventId);
+    }
+}
