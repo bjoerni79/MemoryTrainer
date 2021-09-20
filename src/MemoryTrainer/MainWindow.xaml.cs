a94b1ed66c9bcf4e56763c3ab4c97aae495db728
Björn Hillebrand <bjoern_hillebrand@hotmail.de>
2021-09-20 18:58:36 +0200

    Refactoring: Extracting the IUiService code to a class and clean up the code behind of the Main Window

--- src/MemoryTrainer/MainWindow.xaml.cs
+++ src/MemoryTrainer/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace MemoryTrainer
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : RibbonWindow, IUiService
+    public partial class MainWindow : RibbonWindow
     {
 
 
@@ -42,136 +42,12 @@ namespace MemoryTrainer
             //
             // The IContainer resources is available via the application wide App.xaml
             //
+            var uiService = new UiService(this, tabControl);
+
             var container = FindResource("Container") as IContainer;
-            container.AddService(typeof(IUiService), this);
+            container.AddService(typeof(IUiService), uiService);
 
         }
-
-        public IPage ShowResultOverview(string id)
-        {
-            var headerName = "Result Overview";
-            var page = new ResultPage(id);
-
-            var viewFound = false;
-            foreach (TabItem item in tabControl.Items)
-            {
-                // let's do some old school coding ...
-                if (item.Name.Equals(id))
-                {
-                    viewFound = true;
-                    item.Focus();
-                    break;
-                }
-            }
-
-            if (!viewFound)
-            {
-                CreateTab(page, id, headerName);
-            }
-
-            return page;
-        }
-
-        public IPage CreatePage(PageSelection element, string pageId)
-        {
-            UserControl page;
-            string headerName;
-
-            switch(element)
-            {
-                case PageSelection.CardGame:
-                    page = new CardGame(pageId);
-                    headerName = "PAO Card Game";
-                    break;
-                case PageSelection.ShowHelp:
-                    headerName = "Help";
-                    page = new Help(pageId);
-                    break;
-                case PageSelection.NumberGame:
-                    page = new NumbersGame(pageId);
-                    headerName = "Number Game";
-                    break;
-
-                default:
-                    throw new Exception("Unknown page selection detected");
-            }
-
-            CreateTab(page, pageId, headerName);
-
-            return page as IPage;
-        }
-
-        private void CreateTab(UserControl page, string id, string headerName)
-        {
-            // Create a new page {element
-            var tabItem = new TabItem() { Header = headerName };
-            tabItem.Content = page;
-            tabItem.Name = id;
-            tabItem.HeaderTemplate = FindResource("tabItemTemplate") as DataTemplate;
-
-            tabControl.Items.Add(tabItem);
-            tabItem.Focus();
-        }
-
-        public bool ShowDialog(string message, string caption)
-        {
-            MessageBox.Show(message, caption);
-            return true;
-        }
-
-        public string ShowOpenFileDialog()
-        {
-            var dialog = new OpenFileDialog();
-            dialog.Filter = "PAO Results|*.json|All Files|*.*";
-            var result = dialog.ShowDialog(this);
-            if (result.HasValue && result.Value)
-            {
-                var filename = dialog.FileName;
-                return filename;
-            }
-
-            return null;
-        }
-
-        public string ShowSaveFileDialog()
-        {
-            var dialog = new SaveFileDialog();
-            dialog.Filter = "PAO Results|*.json|All Files|*.*";
-            var result = dialog.ShowDialog(this);
-            if (result.HasValue && result.Value)
-            {
-                var filename = dialog.FileName;
-                return filename;
-            }
-
-            return null;
-        }
-
-        public void Close(string pageId)
-        {
-            TabItem currentItem = null;
-
-            var items = tabControl.Items;
-            foreach (TabItem item in items)
-            {
-                if (item.Name.Equals(pageId))
-                {
-                    currentItem = item;
-                    break;
-                }
-            }
-
-            if (currentItem != null)
-            {
-                // Remove the TabItem
-                tabControl.Items.Remove(currentItem);
-
-                // Remove the associated view model
-                var containerHelper = FacadeFactory.Create();
-                containerHelper.Remove(pageId);
-            }
-        }
-
 
     }
 }
