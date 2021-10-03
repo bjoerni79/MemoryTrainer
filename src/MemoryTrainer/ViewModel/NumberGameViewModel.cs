d43e0a2f6c67a98d335dbf83191ac4199dcc9d16
Björn Hillebrand <bjoern_hillebrand@hotmail.de>
2021-10-03 09:27:57 +0200

    - Numbers can be removed from the list - UI improvements

--- src/MemoryTrainer/ViewModel/NumberGameViewModel.cs
+++ src/MemoryTrainer/ViewModel/NumberGameViewModel.cs
@@ -33,15 +33,26 @@ namespace MemoryTrainer.ViewModel
             GenerateCreditCard = new DefaultCommand(OnGenerateVisaCard);
             GenerateIban = new DefaultCommand(OnGenerateIban);
             GenerateNumber = new DefaultCommand(OnGenerateNumber);
+
+            Start = new DefaultCommand(OnStart);
+            Resolve = new DefaultCommand(OnResolve);
+
+            RemoveNumber = new DefaultCommand(OnRemoveNumber);
         }
 
         public ObservableCollection<NumberComplexityEnum> ComplexityLevel { get; private set; }
 
         public NumberComplexityEnum SelectedComplexityLevel { get; set; }
 
+        public Number SelectedNumber { get; set; }
+
         public bool CharacterEnabled { get; set; }
 
         public ObservableCollection<Number> NumberCollection { get; private set; }
+
+        public IRefreshCommand Start { get; private set; }
+
+        public IRefreshCommand Resolve { get; private set; }
 
         public IRefreshCommand Close { get; private set; }
 
@@ -51,6 +62,18 @@ namespace MemoryTrainer.ViewModel
 
         public IRefreshCommand GenerateNumber { get; private set; }
 
+        public IRefreshCommand RemoveNumber { get; private set; }
+
+        private void OnStart()
+        {
+            //TODO: Set all number games to start
+        }
+
+        private void OnResolve()
+        {
+            //TODO: Resolve all of them
+        }
+
         private void OnGenerateIban()
         {
             var number = numberGenerator.CreateIban();
@@ -59,14 +82,46 @@ namespace MemoryTrainer.ViewModel
 
         private void OnGenerateNumber()
         {
-            var number = numberGenerator.CreateNumber(5, true);
-            AddEntry(number);
+            int length;
+            switch (SelectedComplexityLevel)
+            {
+                case NumberComplexityEnum.Simple:
+                    length = 5;
+                    break;
+                case NumberComplexityEnum.Medium:
+                    length = 10;
+                    break;
+                case NumberComplexityEnum.Complex:
+                    length = 15;
+                    break;
+                default:
+                    length = 0;
+                    break;
+            }
+
+            if (length != 0)
+            {
+                var areCharsIncluded = CharacterEnabled;
+                var number = numberGenerator.CreateNumber(5, areCharsIncluded);
+                AddEntry(number);
+            }
         }
 
         private void OnGenerateVisaCard()
         {
             var creditCardNumber = numberGenerator.CreateCreditCard();
             AddEntry(creditCardNumber);
+        }
+
+        private void OnRemoveNumber()
+        {
+            var currentSelection = SelectedNumber;
+            if (currentSelection != null)
+            {
+                numberSet.Remove(currentSelection);
+            }
+
+            PopulateView();
         }
 
         private void AddEntry(Number number)
@@ -77,6 +132,8 @@ namespace MemoryTrainer.ViewModel
             var numbers = numberSet.Numbers.OrderBy(n => n.Order);
             PopulateView();
         }
+
+
 
         private void PopulateView()
         {
